195ac10e39fb7a2831587e5efda7a05b068f85f3
Himanshu Sinha <ashusinha1904@gmail.com>
2024-11-03 16:44:57 +0530

    profile page updated

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,40 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView, Pressable } from "react-native";
 import React from "react";
 import tw from "twrnc";
-import { AntDesign } from "@expo/vector-icons";
+import {
+  AntDesign,
+  FontAwesome,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
 
 const Profile = () => {
+  const menuOptions = [
+    {
+      Icon: MaterialIcons,
+      iconName: "switch-account",
+      title: "Switch account",
+      size: 16,
+    },
+    {
+      Icon: AntDesign,
+      iconName: "google",
+      title: "Google Account",
+      size: 15,
+    },
+    {
+      Icon: MaterialCommunityIcons,
+      iconName: "incognito-circle",
+      title: "Turn on Incognito",
+      size: 18,
+    },
+    {
+      Icon: FontAwesome,
+      iconName: "share",
+      title: "Share channel",
+      size: 15,
+    },
+  ];
   return (
     <View style={tw`bg-white py-4`}>
       <View style={tw`px-3 flex-row gap-x-3 items-center`}>
@@ -22,6 +53,37 @@ const Profile = () => {
               <AntDesign name="right" size={15} color="gray" />
             </View>
           </View>
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={tw`px-3 gap-x-1.5 mt-5`}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {menuOptions.map((menuOption) => {
+          return (
+            <View
+              style={tw`bg-[#f2f2f2] py-1.5 px-3.5 rounded-full flex-row gap-x-2 items-center`}
+              key={menuOption.title}
+            >
+              <menuOption.Icon
+                name={menuOption.iconName as never}
+                size={menuOption.size}
+                color="black"
+              />
+              <Text style={tw`font-semibold`}>{menuOption.title}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+
+      <View style={tw`mt-6 px-4`}>
+        <View style={tw`flex-row justify-between items-center`}>
+          <Text style={tw`text-lg font-bold`}>History</Text>
+          <Pressable>
+            <Text>View all</Text>
+          </Pressable>
         </View>
       </View>
     </View>
